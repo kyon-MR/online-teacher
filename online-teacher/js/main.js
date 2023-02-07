da1ded2f66b65b5e5c4d846e53c65007e3973f6b
kyon-MR <108559388+kyon-MR@users.noreply.github.com>
2023-02-07 16:19:02 +0900

    Add files via upload

--- online-teacher/js/main.js
+++ online-teacher/js/main.js
@@ -19,14 +19,14 @@
     $('header #sp-menu').click(function() {
       $(this).hide();
       $('header #menu').show().addClass("show");
-      $('header #modal').animate({opacity: 1}, 500).show();
+      $('header #modal').addClass("mdShow").animate({opacity: 1}, 500).show();
       $('header #sp-close').show();
     });
 
     $('header #sp-close').click(function() {
       $(this).hide();
-      $('header #menu').hide().removeClass("show");
-      $('header #modal').animate({opacity: 0}, 500);
+      $('header #menu').hide();
+      $('header #modal').removeClass("mdShow").hide();
       $('header #sp-menu').show();
     });
 
